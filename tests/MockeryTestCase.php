dd959a2af23d907eb9b64c01c64c6bb81169f06e
Denis Yanchevskiy <denis@denisco.pro>
2026-02-26 23:55:42 +0300

    Corrects

--- tests/MockeryTestCase.php
+++ tests/MockeryTestCase.php
@@ -10,41 +10,4 @@ abstract class MockeryTestCase extends FundrikTestCase {
 
 	// phpcs:ignore SlevomatCodingStandard.Classes.TraitUseSpacing.IncorrectLinesCountAfterLastUse
 	use MockeryPHPUnitIntegration;
-
-	protected function array_has( array $expected ): \Mockery\Matcher\Closure {
-
-		return \Mockery::on(
-			static function ( $data ) use ( $expected ) {
-
-				if ( ! is_array( $data ) ) {
-					return false;
-				}
-
-				foreach ( $expected as $k => $v ) {
-
-					if ( ! array_key_exists( $k, $data ) ) {
-						return false;
-					}
-
-					$actual = $data[ $k ];
-
-					if ( $v instanceof \Closure ) {
-
-						if ( ! $v( $actual ) ) {
-							return false;
-						}
-					} elseif ( $v instanceof \Mockery\Matcher\MatcherInterface ) {
-
-						if ( ! $v->match( $actual ) ) {
-							return false;
-						}
-					} elseif ( $actual !== $v ) {
-						return false;
-					}
-				}
-
-				return true;
-			},
-		);
-	}
 }
